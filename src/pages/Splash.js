42c9f3ecfa15e6e725b60c25e46467bf1de7159d
underdogdude <underdogbright@gmail.com>
2020-08-23 17:02:02 +0800

    ADD bg

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -2,20 +2,27 @@ import React, { Component } from "react";
 import {
     Link
 } from "react-router-dom";
+import Logo from "../img/logo.png";
 
 class Splash extends Component { 
     render() { 
         return (
-            <div className="d-flex align-items-center justify-content-center flex-column">
-              <h1 className="splash__title">
-                Open Word
-              </h1>
-              <Link to="/catagory">
-                <button className="btn btn-primary">
-                    START!  
-                </button>
-              </Link>
+          <div className="container">
+            
+            <div className="d-flex align-items-center justify-content-center flex-column h-100">
+                <img src={Logo} className="logo" width={"100px"}/>
+
+                <h1 className="headline">
+                  HELLO WORD
+                </h1>
+
+                <Link to="/catagory">
+                  <button className="btn btn-primary">
+                      START!  
+                  </button>
+                </Link>
             </div>
+          </div>
         )
     }
 }
