f2eb18eca11d1e17bc91872745b1d2b87347cf9a
underdogdude <underdogbright@gmail.com>
2020-09-13 15:53:48 +0800

    #4

--- src/App.js
+++ src/App.js
@@ -7,25 +7,9 @@ import {
 import Splash from "./pages/Splash"
 import Catagory from "./pages/Catagory";
 import Word from "./pages/Word";
-import "./Lineliff-script.js";
+// import "./Lineliff-script.js";
 
 class App extends Component {
-    // constructor(props) {
-    //   super(props);
-    //   this.state = {
-    //     data: []
-    //   };
-    // }
-
-    // componentDidMount() {
-    //   let url = "https://sheet.best/api/sheets/bf8b383a-e9d9-4a78-9dfd-46d997c3ec2e";
-    //   axios.get(url).then((res) => {
-    //    this.setState({
-    //      data: res.data
-    //    })
-    //   });
-    // }
-
     render() {
         return (
             <Router>
