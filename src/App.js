24933cf7a002e5e4a43b66bb724b0ab28d17ac81
underdogdude <underdogbright@gmail.com>
2020-09-13 16:32:52 +0800

    #7

--- src/App.js
+++ src/App.js
@@ -9,20 +9,17 @@ import Splash from "./pages/Splash"
 import Catagory from "./pages/Catagory";
 import Word from "./pages/Word";
 // import "../Lineliff-script.js";
-import { createBrowserHistory } from "history";
-
-const history = createBrowserHistory();
 
 class App extends Component {
     render() {
         return (
-          <Router history={history}>
+          // <Router history={history}>
               <Switch>
                 <Route exact path="/" component={Splash} />
                 <Route path="/catagory" component={Catagory} />
                 <Route path="/word/:catId" component={Word} />
               </Switch>
-            </Router>
+            // </Router>
         );
     }
 }
